3c852828301df26212ac5234e033a2ceedd05c01
Jennifer <32230988+jlweichen@users.noreply.github.com>
2018-02-11 21:23:47 -0500

    Add files via upload
    Python list of team names, coach names, player names, team nicknames, cheers, etc.

---
+++ superbowllii/footballterms.py
@@ -0,0 +1,3 @@
+termlist = ['patriots', 'eagles', 'brady', 'foles', 'wentz', 'ajayi', 'go patriots', 
+'belichick', 'pedersen', 'pats', 'flyeaglesfly', 'goeagles',
+'gopats','gopatriots', 'jalen ramsey','go pats', 'super bowl', 'us bank stadium', 'gronk']
